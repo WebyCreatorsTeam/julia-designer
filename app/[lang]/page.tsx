a4e3385941985e12ade3ee017712f724205a7825
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-10-03 11:53:28 +0300

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,7 +2,7 @@
 import { getDictionary } from "./dictionaries";
 import Image from "next/image";
 import hero from './images/hero-big.png'
-import Hero from "./components/Hero/Hero";
+// import Hero from "./components/Hero/Hero";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function Home({ params: { lang } }: { params: { lang: string }; }) {
