94e35860ac988019bfa49481660970e63ef79089
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-10-04 18:07:48 +0300

    Update page.tsx

--- app/[lang]/project/[projectName]/page.tsx
+++ app/[lang]/project/[projectName]/page.tsx
@@ -6,6 +6,21 @@ const page = async ({ params: { lang, projectName } }: { params: { lang: string,
     const dict = await getDictionary(lang)
     const pro = dict.project.find((pro) => pro.title === title)
     // console.log(pro)
+
+    if (!pro) {
+        // Handle the case when no project is found
+        return <section
+            // className='pageWidth'
+            style={{
+                height: '65vh',
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+            <h1>Page not found</h1>
+        </section>
+    }
+
     return (
         <section className='pageWidth'>
             <h1>{pro.title}</h1>
